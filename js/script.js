1bea712d61274907e8b83c0941ea5ccde0939253
AdrianaPerezL <112712237+AdrianaPerezL@users.noreply.github.com>
2023-02-24 23:56:07 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,39 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                blue1: "#001524",
+                blue2: "#15616d",
+                white1: "#ffecd1",
+                gray1: '#BFB6AE',
+                yellow1: "#ffd100",
+                black1: "#202020",
+                black2: "#000000",
+                red1: "#8C0327",
+                orange1: "#BF613F",
+                brown1: "#59342C",
+                green1: "#A69B03"
+              },
+
+              fontFamily: {
+                font1: ['Tilt Neon'],
+                font2: ['Open Sans']
+              },
+
+              spacing: {
+                '1': '50px',
+                '646': '646px',
+                '1068': '1068px',
+                '1920': '1920px',
+                '200': '200px',
+                '350': '350px',
+                '8': '-8px',
+                '20': '-20px',
+                '128': '128px',
+                '73': '73px',
+                '143': '143px',
+                '20': '20px'
+              }
+        }
+    }
+}
